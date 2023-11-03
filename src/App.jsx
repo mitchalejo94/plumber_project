c82bd02f08f92d37a541dc73b07e113e78062bc8
Mitchel Alejo <michalejo94@gmail.com>
2023-11-03 14:12:49 -0500

    Fixed:added contact page and fixed the route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer";
 import NavigationBar from "./components/NavigationBar";
 import About from "./components/about/About";
+import Contact from "./components/contact/contact";
 import "./scss/main.scss";
 import "./App.css";
 
 function App() {
-  const [mainSection, setMainSection] = useState("home");
-  const toggleSection = (section) => {
-    setMainSection(section);
-  };
-
   return (
-    <>
-      <Header />
-      <NavigationBar />
-      {mainSection === "home" && <Home />}
-      {mainSection === "about" && <About />}
-      {/* {currentSection === "contact" && <Contact />} */}
-      {/* <Home />
-      <About /> */}
-      <Footer />
-    </>
+    <Router>
+      <>
+        <Header />
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </>
+    </Router>
   );
 }
 
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,9 @@
+import "./contact.css";
+const Contact = () => {
+  return (
+    <div id="footer">
+      <h1 id="">contact page</h1>
+    </div>
+  );
+};
+export default Contact;
